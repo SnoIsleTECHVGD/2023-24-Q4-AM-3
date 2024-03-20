03cef44f2d6b56266a0e5a8f4196f6e97ef1bd85
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2024-03-20 07:58:28 -0700

    nope

--- Assets/Jaden/Movement.cs
+++ Assets/Jaden/Movement.cs
@@ -13,7 +13,21 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello");
-        print("Hello");
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0,.01f,0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-.01f,0,0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0,-.01f,0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(.01f,0,0));
+        }
     }
 }
