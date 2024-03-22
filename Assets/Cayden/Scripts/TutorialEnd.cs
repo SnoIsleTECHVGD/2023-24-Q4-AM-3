10bdd5be2d2f696df742e68f543d1d1c20e98819
MUKILTEO\1717590 <1717590@mukilteo.wednet.edu>
2024-03-22 10:01:31 -0700

    tile p;alleten nown nroky

---
+++ Assets/Cayden/Scripts/TutorialEnd.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialEnd : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "TutorialEnd")
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
