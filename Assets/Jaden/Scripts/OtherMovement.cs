68597111b62e2f3850120d27c6d778420dfa1646
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2024-03-26 08:54:36 -0700

    dog incorporated

---
+++ Assets/Jaden/Scripts/OtherMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+public class OtherMovement : MonoBehaviour
+{
+    public float speed, dashCooldown, cooldown;
+    public bool dashing;
+    public bool canMove = true;
+    private Vector2 movement2;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        canMove = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (canMove)
+        {
+            movement2.x = Input.GetAxisRaw("Horizontal");
+            movement2.y = Input.GetAxisRaw("Vertical");
+            movement2.Normalize();
+            rb.velocity = movement2 * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (cooldown <= 0 && dashing == false)
+            {
+                cooldown = dashCooldown;
+                StartCoroutine(Dash());
+            }
+        }
+        cooldown -= .01f;
+    }
+    private IEnumerator Dash()
+    {
+        dashing = true;
+        canMove = false;
+        rb.velocity = new Vector2(rb.velocity.x * 2, rb.velocity.y * 2);
+        yield return new WaitForSeconds(.5f);
+        rb.velocity = new Vector2(rb.velocity.x / 2, rb.velocity.y / 2);
+        canMove = true;
+        dashing = false;
+    }
+}
--- Assets/Jaden/Scripts/PlayerAttack.cs
+++ Assets/Jaden/Scripts/PlayerAttack.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,8 @@ public class PlayerAttack : MonoBehaviour
     public GameObject weapon;
     public GameObject mainCamera;
     public Camera cam;
+    public bool swinging;
+    public Quaternion rotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +21,20 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        
+        if (Input.GetMouseButton(0) && !swinging)
         {
             Vector2 myPos = new(transform.position.x, transform.position.y);
             Vector2 direction = GetMouseDirection() - myPos;
             direction.Normalize();
-            Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg );
-            weapon.transform.RotateAround(transform.position, Vector3.back, Mathf.Abs(rotation.z - weapon.transform.rotation.z) * 9);
+            rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            weapon.transform.rotation = rotation;
+            StartCoroutine(Swing());
         }
-        Debug.Log(GetMouseDirection());
+        if (swinging)
+        {
+            weapon.transform.RotateAround(transform.position, Vector3.back, Mathf.Abs(rotation.z - weapon.transform.rotation.z));
+        }
     }
     Vector2 GetMouseDirection()
     {
@@ -34,6 +42,12 @@ public class PlayerAttack : MonoBehaviour
         dir = cam.ScreenToWorldPoint(dir);
 
         return new Vector2(dir.x, dir.y);
+    }
+    IEnumerator Swing()
+    {
+        swinging = true;
+        yield return new WaitForSeconds(2);
+        swinging = false;
     }
 
 }
