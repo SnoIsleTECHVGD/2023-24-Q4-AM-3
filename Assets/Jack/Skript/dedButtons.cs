d3da78b1d851186435c21187a9b46b93e0cb84b6
MUKILTEO\1717858 <1717858@mukilteo.wednet.edu>
2024-04-12 09:51:44 -0700

    helth❤

---
+++ Assets/Jack/Skript/dedButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class dedButtons : MonoBehaviour
+{
+    void Update()
+    {
+        if (1 == 2)
+        {
+            //Destroy(gameObject);
+        }
+    }
+    public void Retry()
+    {
+        SceneManager.LoadScene("Tutorial Level");
+    }
+    public void Title()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
